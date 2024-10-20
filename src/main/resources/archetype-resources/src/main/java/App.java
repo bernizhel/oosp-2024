cbbbcfecc23431b09aa45103a5691dac9b72ad45
Danil Kosenko <detrop.1995@yandex.ru>
2024-10-20 20:29:36 +0300

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.vsuet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
